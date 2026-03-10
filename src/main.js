a09ff4e0bb938092d9321ee8ac4482d7ddcd12e9
Peter Anga <angapeter5@gmail.com>
2026-03-10 16:14:32 +0100

    Fix file loading issue

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ const path = require('path');
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
-    height: 800
+    height: 800,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
   });
 
-  win.loadFile(path.resolve(__dirname, 'index.html'));
+  const startUrl = path.join(__dirname, 'index.html');
+  win.loadURL(`file://${startUrl}`);
+
+  win.webContents.openDevTools(); // remove later if you want
 }
 
-app.whenReady().then(() => {
-  createWindow();
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
