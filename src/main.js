a5753746219b8cb5e0d9b1433434e9fcd1eb5b98
Peter Anga <angapeter5@gmail.com>
2026-03-10 17:21:21 +0100

    Fix index.html loading path

--- src/main.js
+++ src/main.js
@@ -4,13 +4,10 @@ const path = require('path');
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
-    height: 800,
-    webPreferences: {
-      contextIsolation: true
-    }
+    height: 800
   });
 
-  win.loadFile('src/index.html');
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
 
 app.whenReady().then(createWindow);
